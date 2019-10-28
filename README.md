f4d444007ff21e8e470082683da335b7ef2c429b
Johann Prell <johann.prell@gmail.com>
2019-10-28 13:39:00 +0100

    simple three scene and loop

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # three-parcel-boilerplate
-starting point for three.js project using parcel to bundle and serve
+A bare-bones starting point for experimenting with three.js in a modern js environment.
+
+Whith node.js & yarn installed, run:
+```
+yarn
+```
+to install dependencies and
+```
+yarn dev
+```
+to run local server.
+
+## About
+[Three.js](https://threejs.org/) is a library for 3D/WebGL in the browser.
+
+[Parcel](https://parceljs.org/) is an easy-to-use bundler for modern js apps.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-alert('hello world')
+import * as THREE from 'three'
+
+let scene
+let camera
+let renderer
+let cube
+
+init()
+function init() {
+    scene = new THREE.Scene()
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+    renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xff00ff } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
+    camera.position.z = 5;
+
+    update()
+}
+function update() {
+    requestAnimationFrame(update)
+    renderer.render(scene, camera)
+    // Cube animation as example. Replace with array of update functions or similar.
+    animateCube()
+}
+
+function animateCube() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+}
